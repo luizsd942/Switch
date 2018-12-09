88e7889926c97f92922ed5fe0e983eaa3b53664d
luizsd942 <luizsd942@gmail.com>
2018-12-09 14:43:59 -0200

    Classe Usuário  organização de diretórios;

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Switch.Domain.Enums;
+
+namespace Switch.Domain.Entities
+{
+    class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }        
+        public SexoEnum Sexo { get; set; }
+        public string UrlFoto { get; set; }
+
+    }
+}
---
+++ Switch.Infra.Data/Context/SwitchContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Infra.Data.Context
+{
+    public class SwitchContext : DbContext
+    {
+    }
+}
