ccdee84874cdf12eff869765aef12bf0f5326ad6
luizsd942 <luizsd942@gmail.com>
2018-12-14 22:41:31 -0200

    Classes de Postagem e Status Relacionamentos; Migrations;

---
+++ Switch.Infra.Data/Config/PostagemConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Switch.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Infra.Data.Config
+{
+    public class PostagemConfiguration : IEntityTypeConfiguration<Postagem>
+    {
+        public void Configure(EntityTypeBuilder<Postagem> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.DataPublicacao).IsRequired().HasColumnName("Data_Publicacao");
+            builder.Property(p => p.Texto).HasColumnName("Texto_Publicacao").IsRequired();            
+        }
+    }
+}
