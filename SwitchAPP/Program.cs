967cd61999840b6c74e64f58043a9b7972e4f6cb
luizsd942 <luizsd942@gmail.com>
2019-01-21 21:29:26 -0200

    Criação de Logs e projeto console para testes;

---
+++ SwitchAPP/Program.cs
@@ -0,0 +1,56 @@
+﻿using Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Logging;
+using Switch.Domain.Entities;
+using Switch.Infra.Data.Config;
+using Switch.Infra.Data.Context;
+using System;
+using System.Linq;
+
+namespace SwitchAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = NovoUsuario("3");
+
+            var optionBuilder = new DbContextOptionsBuilder<SwitchContext>().EnableSensitiveDataLogging();
+            optionBuilder.UseLazyLoadingProxies();
+            optionBuilder
+                .UseMySql("Server=localhost;userid=root;Database=SwitchDB", m => m.MigrationsAssembly("Switch.Infra.Data"));
+
+            try
+            {
+                using (var contexto = new SwitchContext(optionBuilder.Options))
+                {
+                    contexto.GetService<ILoggerFactory>().AddProvider(new Logger());
+
+                    var resultado = contexto.Usuarios.ToList();
+                    
+
+                    //Console.WriteLine("Usuário add com sucesso!");
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+
+        private static Usuario NovoUsuario(string valor)
+        {
+            return new Usuario
+            {
+                Nome = "Usuario"+valor,
+                Email = "Email"+valor,
+                DataNascimento = new DateTime(2002, 2, 5),
+                SobreNome = "Sobrenome "+valor,
+                Senha = "senha"+valor
+            };
+        }
+    }
+}
